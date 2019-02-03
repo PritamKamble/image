bf8bd3181d94eda04c5a565a1622ccf01a744e01
Pritam <7276279026.pk@gmail.com>
2019-02-03 18:28:35 +0530

    4 features included
    1.sharpness
    2.color Enhancement
    3.brightness
    4.contrast

--- ImageEnhance.py
+++ ImageEnhance.py
@@ -8,8 +8,23 @@ enhancer = ImageEnhance.Sharpness(image)
 
 #for i in range(8):
 factor = 8 / 4.0
-image=enhancer.enhance(factor).show("Sharpness %f" % factor)
+sharpimage=enhancer.enhance(factor)#.show("Sharpness %f" % factor)
+#Image._show(sharpimage)
 
-enhancer2=ImageEnhance.Color(image)
-factor = 8 / 4.0
-image=enhancer.enhance(factor).show("color %f" % factor)
+
+enhancer = ImageEnhance.Color(sharpimage)
+#enhancer = ImageEnhance.Sharpness(image)
+# Both Color and Sharpness has same 'enhance(factor)' method.
+factor = 1.0*1.5
+colored=enhancer.enhance(factor)#.show('colored',"color %f" % factor)
+#Image._show(colored)
+
+enhancer=ImageEnhance.Brightness(colored)
+factor=1.2
+bright=enhancer.enhance(factor)
+#Image._show(bright)
+
+enhancer=ImageEnhance.Contrast(bright)
+factor=1.1223
+contra=enhancer.enhance(factor)
+Image._show(contra)
