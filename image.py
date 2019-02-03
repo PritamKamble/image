d8a60dcd3648a0e95dba20c63bf356d639b03a1b
Pritam <7276279026.pk@gmail.com>
2019-02-03 21:49:59 +0530

    initial commit in 4features

--- image.py
+++ image.py
@@ -2,16 +2,16 @@
 from PIL import Image
 
 def reduce():
-        img = Image.open('book.jpg').convert('L')  # convert image to 8-bit grayscale
+        img = Image.open('olay.jpg').convert('L')  # convert image to 8-bit grayscale
         basewidth , height = img.size
         basewidth=50
         wpercent = (basewidth/float(img.size[0]))
         hsize = int((float(img.size[1])*float(wpercent)))
         img = img.resize((basewidth,hsize), Image.ANTIALIAS)
-        img.save('booksmall.jpg')
+        img.save('small.jpg')
 
 
-        img = Image.open('booksmall.jpg').convert('L')  # convert image to 8-bit grayscale
+        img = Image.open('small.jpg').convert('L')  # convert image to 8-bit grayscale
         WIDTH, HEIGHT = img.size
 
         data = list(img.getdata()) # convert image data to a list of integers
@@ -26,11 +26,11 @@ def reduce():
             print(' '.join('{:3}'.format(value) for value in row))
 
         #Here's another more compact representation.
-        chars = '@%#*+=-:. '  # Change as desired.
-        scale = (len(chars)-1)/255.
-        print()
-        for row in data:
-                print(' '.join(chars[int(value*scale)] for value in row))
+        #chars = '@%#*+=-:. '  # Change as desired.
+        #scale = (len(chars)-1)/255.
+        #print()
+        #for row in data:
+        #        print(' '.join(chars[int(value*scale)] for value in row))
 
 if __name__=="__main__":
         reduce()
